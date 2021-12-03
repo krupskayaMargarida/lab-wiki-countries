0c3607d2f5bf0be0105f50ef68a4ed1c7fd0a8b0
Margarida Krupsaya <gomesfmargarida@gmail.com>
2021-12-03 09:54:31 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import CountryDetails from './components/CountryDetails';
+import CountriesList from './components/CountriesList';
+import countriesData from './countries.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div class="container">
+        <div class="row">
+          <CountriesList countriesData={countriesData} />
+          <Routes>
+            <Route
+              path="/:alpha3Code"
+              element={<CountryDetails countriesData={countriesData} />}
+            />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
